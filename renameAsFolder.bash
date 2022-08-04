b4f7cd25a60041a80982acc0e5e849a00f9cacf7
Simone Checchia <62959575+techkek@users.noreply.github.com>
2022-08-04 17:00:46 +0200

    Fixed 3 errors
    1. If the bash script was already executed, the script return an error, line 16's if prevent this.
    2. Fixed #1 at line 8
    3. Fixed weird things happen when there were spaces in the folder name

--- renameAsFolder.bash
+++ renameAsFolder.bash
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 folders=()
-for d in */ ;
+for d in */
 do
-  folders+=( $d )
+  folders+=( "$d" )
 done
-
-for i in "$folders"
+for i in "${folders[@]}"
 do
-  for e in $i*
+  for e in "$i"*
   do
-    filename=$e
+    filename="$e"
   done
   extension=${filename##*.}
   n=${i::-1}
-  mv "$filename" "$i$n.$extension"
+  if [ "$filename" != "$i$n.$extension" ]; then
+    mv "$filename" "$i$n.$extension"
+  fi
 done
